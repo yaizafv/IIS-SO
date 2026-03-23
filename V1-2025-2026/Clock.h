5e0e7e68a88830c7fc2af05e262898ac41c57c8b
yaiza <yaizafv2004@gmail.com>
2026-03-23 17:47:12 +0100

    Delete V1-2025-2026 directory

--- V1-2025-2026/Clock.h
+++
@@ -1,8 +0,0 @@
-#ifndef Clock_H
-#define Clock_H
-
-// Functions prototypes
-void Clock_Update();
-int Clock_GetTime();
-
-#endif
--- V1-2025-2026/Wrappers.h
+++
@@ -1,9 +0,0 @@
-#ifndef WRAPPERS_H
-#define WRAPPERS_H
-
-void __wrap_Processor_InstructionCycleLoop();
-int __wrap_Processor_FetchInstruction();
-void __wrap_Processor_DecodeAndExecuteInstruction();
-void __wrap_OperatingSystem_InterruptLogic(int);
-
-#endif
